10e66340322d3fc8b9b187162fcc95ce5ccc0916
Hans Llanos <hans.llanos@gmail.com>
2024-08-15 16:19:31 -0500

    creacion de hooks

--- practica/src/context/PokemonProvider.jsx
+++ practica/src/context/PokemonProvider.jsx
@@ -2,8 +2,15 @@ import React, { useState, useEffect } from 'react'
 import { PokemonContext } from "./PokemonContext";
 
 export default function PokemonProvider({children}) {
-
+    const [allPokemon, setAllPokemon] = useState([]);
+    const [globalPokemon, setGlobalPokemon] = useState([]);
     const [offset, setOffset] = useState(0);
+
+    //Utilizar CustomHook  - Use Form
+    
+    //Estados carga
+    const[loading, setLoading] = useState(true);
+    const[active, setActive] = useState(false);
 
     //Llamar a 50 pokemones a la API
     const getAllPokemon = async(limit = 50)=>{
@@ -19,11 +26,42 @@ export default function PokemonProvider({children}) {
         })
 
         const results = await Promise.all(promises);
-        console.log(results);
+        setAllPokemon([...allPokemon, ...results]);
+        setLoading(false);
+    }
+
+    //  Llamar todos los pokemones
+    const getGlobalPokemons = async()=> {
+        const baseURL = 'https://pokeapi.co/api/v2/';
+
+        const res = await  fetch(`${baseURL}pokemon?limit=100000&offset=0`);
+        const data = await res.json();
+        
+        const promises = data.results.map(async(pokemon)=> {
+            const res = await fetch(pokemon.url);
+            const data = await res.json();
+            return data;
+        })
+
+        const results = await Promise.all(promises);
+        setGlobalPokemon(results);
+        setLoading(false);
+    }
+
+    //Llamar Pokemon por id
+    const getPokemonById = async(id) => {
+        const baseURL = 'https://pokeapi.co/api/v2/';
+        const res = await  fetch(`${baseURL}pokemon/${id}`);
+        const data = await res.json();
+        return data;
     }
 
     useEffect(()=> {
         getAllPokemon();
+    },[])
+
+    useEffect(()=> {
+        getGlobalPokemons();
     },[])
 
 
