aa7ecff40ff9cb39516b5e4e73cd91c2432cd492
Hans Llanos <hans.llanos@gmail.com>
2024-08-15 16:00:08 -0500

    consumo de api

--- practica/src/context/PokemonProvider.jsx
+++ practica/src/context/PokemonProvider.jsx
@@ -1,6 +1,25 @@
+import React, { useState, useEffect } from 'react'
 import { PokemonContext } from "./PokemonContext";
 
 export default function PokemonProvider({children}) {
+
+    const [offset, setOffset] = useState(0);
+
+    //Llamar a 50 pokemones a la API
+    const getAllPokemon = async(limit = 50)=>{
+        const baseURL = 'https://pokeapi.co/api/v2/';
+
+        const res = await  fetch(`${baseURL}pokemon?limit=${limit}&offset=${offset}`);
+        const data = await res.json();
+        console.log(data);
+
+    }
+
+    useEffect(()=> {
+        getAllPokemon();
+    },[])
+
+
   return (
     <PokemonContext.Provider value={{ numero:0 }}> {children}</PokemonContext.Provider>
   )
