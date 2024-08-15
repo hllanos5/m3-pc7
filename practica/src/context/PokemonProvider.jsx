7230f9645ebb6425f335285dfa06ef854d07ef21
Hans Llanos <hans.llanos@gmail.com>
2024-08-15 16:03:08 -0500

    manejo de promesas

--- practica/src/context/PokemonProvider.jsx
+++ practica/src/context/PokemonProvider.jsx
@@ -11,8 +11,15 @@ export default function PokemonProvider({children}) {
 
         const res = await  fetch(`${baseURL}pokemon?limit=${limit}&offset=${offset}`);
         const data = await res.json();
-        console.log(data);
+        
+        const promises = data.results.map(async(pokemon)=> {
+            const res = await fetch(pokemon.url);
+            const data = await res.json();
+            return data;
+        })
 
+        const results = await Promise.all(promises);
+        console.log(results);
     }
 
     useEffect(()=> {
